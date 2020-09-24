fec57e39d8e3880d1b587f43e5c0a3804c3c4048
Unknown <99051422@mydavinci.nl>
2020-09-24 09:40:30 +0200

    Initializiation
    Initialization for the html and css, this was done first because:
    
    I already have most of the backend because I'm redoing the challange because I was not happy with the layout that I had.
    For that reason I started prototyping what I actually wanted from the front end, which is just easier to do when you don't have other code messing it up.

---
+++ index.php
@@ -0,0 +1,129 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <link href="css/stylesheet.css" rel="stylesheet" />
+    <title></title>
+</head>
+<body>
+    <div class="list_wrapper">
+        <div class="list">
+            <h4 class="title">List Name</h4>
+            <div class="task_list">
+                <div class="task">
+                    <div class="status not_started">Not started</div>
+                    <div class="description">Task 1, make a detailed description of what you are going to do within this current objective!</div>
+                    <div class="time">5:05-5:10</div>
+                </div>
+                <div class="task">
+                    <div class="status working">Working</div>
+                    <div class="description">Task 2, Lorem Impsum</div>
+                    <div class="time">5:05-5:10</div>
+                </div>
+                <div class="task">
+                    <div class="status working">Working</div>
+                    <div class="description">Task 3, Chat some on discord.</div>
+                    <div class="time">5:05-5:10</div>
+                </div>
+                <div class="task">
+                    <div class="status finished">finished</div>
+                    <div class="description">Task 4, Take a short break to stretch your legs and walk to plus.</div>
+                    <div class="time">5:05-5:10</div>
+                </div>
+            </div>
+            <div class="add_task">+ Add another task</div>
+        </div>
+    </div>
+
+    <div class="list_wrapper">
+        <div class="list">
+            <h4 class="title">List Name</h4>
+            <div class="task_list">
+                <div class="task">
+                    <div class="status not_started">Not started</div>
+                    <div class="description">Task 1, make asdfas dfsad fas dfasd fas dfctive!</div>
+                    <div class="time">5:05-5:10</div>
+                </div>
+                <div class="task">
+                    <div class="status not_started">Not started</div>
+                    <div class="description">Task 2, Lord fsa dfa dsem sdf sdfsaImpsum</div>
+                    <div class="time">5:05-5:10</div>
+                </div>
+                <div class="task">
+                    <div class="status working">Working</div>
+                    <div class="description">Task 3, Chat somsa dfa sdfa s asdf asdde on discord.</div>
+                    <div class="time">5:05-5:10</div>
+                </div>
+            </div>
+            <div class="add_task">+ Add another task</div>
+        </div>
+    </div>
+
+    <div class="list_wrapper">
+        <div class="list">
+            <h4 class="title">List Name</h4>
+            <div class="task_list">
+                <div class="task">
+                    <div class="status not_started">Not started</div>
+                    <div class="description">Task 1, make a detailed de fasdfgdfasdf sa dsa dfa sdf asscription of whate!</div>
+                    <div class="time">5:05-5:10</div>
+                </div>
+                <div class="task">
+                    <div class="status finished">Finished</div>
+                    <div class="description">Task 2, Lore fasd fm Impsum</div>
+                    <div class="time">5:05-5:10</div>
+                </div>
+                <div class="task">
+                    <div class="status working">Working</div>
+                    <div class="description">Task 3, Chasd fasd fa sdf sadat soads fasdf asdme on di fasdasd fas df sadfd fasdf asd fasd fasdf scord.</div>
+                    <div class="time">5:05-5:10</div>
+                </div>
+                <div class="task">
+                    <div class="status not_started">Not started</div>
+                    <div class="description">Task 4, Takea sd fa a short break to plus.</div>
+                    <div class="time">5:05-5:10</div>
+                </div>
+                <div class="task">
+                    <div class="status finished">Finished</div>
+                    <div class="description">Task 5, Takea s df sadfsd fa a short break asdf asd fas dfsa d fas to plus.</div>
+                    <div class="time">5:05-5:10</div>
+                </div>
+                <div class="task">
+                    <div class="status working">Working</div>
+                    <div class="description">Task 6, Takea sd fa plus.</div>
+                    <div class="time">5:05-5:10</div>
+                </div>
+                <div class="task">
+                    <div class="status not_started">Not started</div>
+                    <div class="description">Task 1, make a detailed de fasdfgdfasdf sa dsa dfa sdf asscription of whate!</div>
+                    <div class="time">5:05-5:10</div>
+                </div>
+                <div class="task">
+                    <div class="status finished">Finished</div>
+                    <div class="description">Task 2, Lore fasd fm Impsum</div>
+                    <div class="time">5:05-5:10</div>
+                </div>
+                <div class="task">
+                    <div class="status working">Working</div>
+                    <div class="description">Task 3, Chasd fasd fa sdf sadat soads fasdf asdme on di fasdasd fas df sadfd fasdf asd fasd fasdf scord.</div>
+                    <div class="time">5:05-5:10</div>
+                </div>
+                <div class="task">
+                    <div class="status not_started">Not started</div>
+                    <div class="description">Task 4, Takea sd fa a short break to plus.</div>
+                    <div class="time">5:05-5:10</div>
+                </div>
+                <div class="task">
+                    <div class="status finished">Finished</div>
+                    <div class="description">Task 5, Takea s df sadfsd fa a short break asdf asd fas dfsa d fas to plus.</div>
+                    <div class="time">5:05-5:10</div>
+                </div>
+                <div class="task">
+                    <div class="status working">Working</div>
+                    <div class="description">Task 6, Takea sd fa plus.</div>
+                    <div class="time">5:05-5:10</div>
+                </div>
+            </div>
+            <div class="add_task">+ Add another task</div>
+        </div>
+    </div>
+</body>
+</html>
